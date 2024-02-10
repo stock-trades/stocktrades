415e35ea3fb5a8c5d685caf1e7b75a9b4f48b678
stock-trades <varun@whippyinfo.com>
2024-02-10 21:39:20 +0530

    health endpoint

--- src/main/java/io/stocktrades/HealthController.java
+++ src/main/java/io/stocktrades/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
-
+// testing endpoint
   @GetMapping("/health")
   public ResponseEntity<String> testTrades() {
     return ResponseEntity.ok("StockTrades application is running");
