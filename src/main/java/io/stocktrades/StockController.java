e122dd23e5344875d7e7a1707ae4b32298d1bcff
stock-trades <varun@whippyinfo.com>
2024-02-29 17:22:23 +0530

    adding redirect url and post back url

--- src/main/java/io/stocktrades/StockController.java
+++ src/main/java/io/stocktrades/StockController.java
@@ -1,11 +1,23 @@
 package io.stocktrades;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StockController {
 
+    @GetMapping("/redirect")
+    public ResponseEntity<String> redirect() {
+        System.out.println("Redirect URL");
+        return ResponseEntity.ok("Redirect API Call ");
+    }
 
+    @GetMapping("/postback")
+    public ResponseEntity<String> postbackOrderCompletionUpdates() {
+        System.out.println("GET call Postback URL");
+        return ResponseEntity.ok("Postback API Call ");
+    }
 
 
 }
