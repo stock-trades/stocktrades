6bb0eea9ea288d39ca7916d26290f9886cde5825
stock-trades <varun@whippyinfo.com>
2024-03-02 08:57:13 +0530

    intergration with zerodha api

--- src/main/java/io/stocktrades/StocktradesIoApplication.java
+++ src/main/java/io/stocktrades/StocktradesIoApplication.java
@@ -3,16 +3,15 @@ package io.stocktrades;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "io.stocktrades")
 public class StocktradesIoApplication {
 
-  public static final String APP_EXTERNAL_PROPERTIES_CONFIG=
-          "spring.config.location=classpath:/"+",classpath:application.properties";
+  public static final String APP_EXTERNAL_PROPERTIES_CONFIG =
+      "spring.config.location=classpath:/" + ",classpath:application.properties";
 
   public static void main(String[] args) {
     new SpringApplicationBuilder(StocktradesIoApplication.class)
-            .properties(APP_EXTERNAL_PROPERTIES_CONFIG)
-            .run(args);
+        .properties(APP_EXTERNAL_PROPERTIES_CONFIG)
+        .run(args);
   }
 }
-
--- src/main/java/io/stocktrades/controller/StockController.java
+++ src/main/java/io/stocktrades/controller/StockController.java
@@ -1,6 +1,8 @@
 package io.stocktrades.controller;
 
 import io.stocktrades.service.StockService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +12,14 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
+@Slf4j
+@RequiredArgsConstructor
 public class StockController {
-  @Value("zerodha.login.url")
-  private String zerodhaLoginUrl;
 
-  private StockService stockService;
+  @Value("${zerodha.login.url}")
+  private  String zerodhaLoginUrl;
+
+  private final StockService stockService;
 
   @GetMapping("/redirect")
   public ResponseEntity<String> redirect() {
@@ -30,6 +35,7 @@ public class StockController {
 
   @GetMapping("/req_token")
   public ResponseEntity<String> getRequestToken() {
+    log.info("zerodha Login url is:{}", zerodhaLoginUrl);
     UriComponents loginUrl = UriComponentsBuilder.fromHttpUrl(zerodhaLoginUrl).build();
     System.out.println("the url is:" + loginUrl);
     return ResponseEntity.ok("Postback API Call ");
